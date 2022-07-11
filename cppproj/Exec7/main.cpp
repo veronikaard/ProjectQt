eaf9a08ee6c778c325f21b807f56b42c24573f8d
Ard <veronika_ardatova@bk.ru>
2022-07-11 14:28:39 +1000

    Update Exec7 str 305 - 328

--- cppproj/Exec7/main.cpp
+++ cppproj/Exec7/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
 using namespace std;
+const int ArSize = 8;
 
+int sum_arr(int arr[], int n);
 int main()
 {
-    cout << "Hello World!" << endl;
+    //Listing 7.5
+    int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
+    cout << cookies << endl;
+    cout << sizeof cookies << endl;
+
+    int sum = sum_arr(cookies, ArSize);
+    cout << "sum = " << sum << endl;
+
     return 0;
+}
+
+int sum_arr(int arr[], int n)
+{
+    int total = 0;
+    for (int i = 0; i < n; i++)
+        total += arr[i];
+    return total;
 }
