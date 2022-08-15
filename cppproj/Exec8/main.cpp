3308b1d98396938b6024167ea43672765cdf92e0
Ard <veronika_ardatova@bk.ru>
2022-08-15 16:56:44 +1000

    Update exec8 str 380-385

--- cppproj/Exec8/main.cpp
+++ cppproj/Exec8/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void swapr(int &a, int &b);
+void swapr(int & a, int &b);
 void swapp(int *p, int *q);
 void swapv(int a, int b);
+
+struct free_throws
+{
+    string name;
+    int made;
+    int attempts;
+    float percent;
+};
+
+void display(const free_throws & ft);
+void set_pc(free_throws & ft);
+free_throws & accumulate(free_throws & taget, const free_throws & source);
 
 int main()
 {
@@ -12,7 +25,7 @@ int main()
 
     //Listing 8.4
     //обмен значениями с помощью ссылок и указателей
-    int wallet1 = 300;
+    /*int wallet1 = 300;
     int wallet2 = 250;
     cout << "wallet1 = $" << wallet1;
     cout << " wallet2 = $" << wallet2 << endl;
@@ -30,9 +43,61 @@ int main()
     cout << "Value:\n";
     swapv(wallet1, wallet2);
     cout << "wallet1 = $" << wallet1;
-    cout << " wallet2 = $" << wallet2 << endl;
+    cout << " wallet2 = $" << wallet2 << endl;*/
+
+    //Listing 8.6
+    //использование ссылок на структуру
+    free_throws one = {"Ifelsa Branch", 13, 14};
+    free_throws two = {"Andor Knott", 10, 16};
+    free_throws three = {"Minnie Max", 7, 9};
+    free_throws four = {"Whily Looper", 5, 9};
+    free_throws five = {"Long Long", 6, 14};
+    free_throws team = {"Throwgoods", 0, 0};
+
+    free_throws dup;
+    set_pc(one);
+    display(one);
+    accumulate(team, one);
+    display(team);
+
+    display(accumulate(team, two));
+    accumulate(accumulate(team, three), four);
+    display(team);
+
+    dup = accumulate(team, five);
+    display(team);
+
+    display(dup);
+    set_pc(four);
+
+    accumulate(dup, five) = four;
+    display(dup);
 
     return 0;
+}
+
+free_throws & accumulate(free_throws &taget, const free_throws &source)
+{
+    taget.attempts += source.attempts;
+    taget.made += source.made;
+    set_pc(taget);
+    return taget;
+}
+
+void set_pc(free_throws &ft)
+{
+    if (ft.attempts != 0)
+        ft.percent = 100.0f * float(ft.made)/float(ft.attempts);
+    else
+        ft.percent = 0;
+}
+
+void display(const free_throws &ft)
+{
+    cout << "Name: " << ft.name << '\n';
+    cout << "Made: " << ft.made << '\t';
+    cout << "Attempts: " << ft.attempts << '\t';
+    cout << "Percent: " << ft.percent << '\n';
 }
 
 void swapr(int &a, int &b)
