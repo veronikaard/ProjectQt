f085cb2fdd081dd71470861f779ef8e41c5ce5d9
Ard <veronika_ardatova@bk.ru>
2022-11-01 16:49:16 +1000

    Update Exec10 str 528-531

--- cppproj/Exec10/main.cpp
+++ cppproj/Exec10/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "stock00.h"
+#include "stack.h"
 
 using namespace std;
 
@@ -26,7 +27,7 @@ int main()
     harry.show();*/
 
     //массив объектов
-    const int ST = 4;
+    /*const int ST = 4;
     Stock stocks[ST] =
     {
         Stock("NanoSmart", 12.5, 20),
@@ -45,7 +46,52 @@ int main()
         top = &top->topval(stocks[i]);
 
     std::cout << "Самый ценный пакет акций:\n";
-    top->show();
+    top->show();*/
+
+    //Тестирование класса Stack
+    using std::cin;
+    using std::cout;
+    Stack st;
+    char ch;
+    unsigned long po;
+    cout << "Введите А - чтобы добавить заказ, Р - обработать заказ,"
+            "Q - завершить.\n";
+    while (cin >> ch && toupper(ch) != 'Q')
+    {
+        while (cin.get() != '\n')
+            continue;
+        if (!isalpha(ch))
+        {
+            cout << '\a';
+            continue;
+        };
+        switch (ch) {
+        case 'A':
+        case 'a':
+            cout << "Введите номер для добавления: ";
+            cin >> po;
+            if (st.isfull())
+                cout << "Стек уже полон\n";
+            else
+               st.push(po);
+            break;
+        case 'P':
+        case 'p':
+            if (st.isempty())
+                cout << "Стек уже пуст\n";
+            else
+            {
+                st.pop(po);
+                cout << "PO #" << po << "удален\n";
+            }
+            break;
+        default:
+            break;
+        }
+        cout << "Введите А - чтобы добавить заказ, Р - обработать заказ,"
+                "Q - завершить.\n";
+    };
+    cout << "Bye!\n";
 
     return 0;
 }
